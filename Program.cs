28640078acd9f252cd73a3ed32057dfb5060b247
joo-github <162849279+joo-github@users.noreply.github.com>
2024-06-28 00:03:53 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using MusicListBusinessLogic;
+using MusicListModel;
+
+namespace MusicUserInterface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MusicService musicService = new MusicService();
+
+            Console.WriteLine("Please log in:");
+            Console.Write("Username: ");
+            string username = Console.ReadLine();
+            Console.Write("Password: ");
+            string password = Console.ReadLine();
+
+            if (musicService.AuthenticateUser(username, password))
+            {
+                Console.WriteLine("Authentication successful!\n");
+
+                var musicList = musicService.GetAllMusic();
+                Console.WriteLine("Music Playlist:");
+                foreach (var music in musicList)
+                {
+                    Console.WriteLine($"Id: {music.Id}, Artist: {music.ArtistName}, Title: {music.SongTitle}, Year: {music.YearComposed}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid username or password.");
+
+            }
+        }
+    }
+}
+    
+
+
